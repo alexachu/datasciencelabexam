be1fb70a3fcc4c1c42054fb944b81a57bd6fb607
alexachu <74653508+alexachu@users.noreply.github.com>
2022-03-30 12:24:22 +0530

    Add files via upload

---
+++ Data Science lab Exam/Arm.py
@@ -0,0 +1,9 @@
+for n in range(0,1001):
+    sum=0
+    temp=n
+    while temp>0:
+        digit = temp % 10
+        sum = sum + digit ** 3
+        temp = temp // 10
+    if(n == sum):
+        print(n)
